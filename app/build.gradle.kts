9557b0eedb6f7e546075a1dbff89e878e9b0b602
Nguyễn Đình Khôi <khoindhe170734@fpt.edu.vn>
2024-06-21 14:23:29 +0700

    update board_item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.material:material:1.4.0")
 }
